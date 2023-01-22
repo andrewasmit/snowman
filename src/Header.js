bb0f9c57a212760fad470b14bf5e4c92151cb52c
andrew <andrewasmit@hotmail.com>
2023-01-22 16:13:52 -0600

    initial commit

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+
+function Header() {
+  return (
+    <header className="App-header">
+        <span>HEADER</span>
+    </header>
+  )
+}
+
+export default Header;
---
+++ src/Snowman.js
@@ -0,0 +1,11 @@
+
+
+function Snowman(){
+    return(
+        <div id='snowman'>
+            <span>THIS IS WHERE THE SNOWMAN GOES</span>
+        </div>
+    )
+}
+
+export default Snowman;
