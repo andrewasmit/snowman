cf0d67cdc724df513ae9079a9a2a52407517fd93
andrew <andrewasmit@hotmail.com>
2023-01-30 21:46:21 -0600

    header updated. not complete, but functioning

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,11 +3,10 @@ import '../App.css';
 
 function Header({ score }) {
   return (
-    <header className="App-header">
-        <span>SNOWMAN</span>
-        <span>
-          <h3 id ="score">SCORE: {score}</h3>
-        </span>
+    <header id="App-header">
+          <img src="./snowman.png"/>
+          <h1 id="title">SNOWMAN</h1>
+        <h3 id="scoreboard">SCORE: {score}</h3>
     </header>
   )
 }
