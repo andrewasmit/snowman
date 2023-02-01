198fb51da6f9ce3acee45fd2f3d084fb525a04d5
andrew <andrewasmit@hotmail.com>
2023-01-31 20:19:23 -0600

    updated package.json

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,9 @@ function Header({ score, highScore }) {
   return (
     <header id="App-header">
         <h5 id ="high-score">HIGH SCORE: {highScore}</h5>
-        <img src="./snowman.png"/>
+        <img src="./snowman.png"alt="snowman"/>
         <h1 id="title">SNOWMAN</h1>
+        <img src="./snowman.png" alt="snowman"/>
         <h3 id="scoreboard">SCORE: {score}</h3>
     </header>
   )
